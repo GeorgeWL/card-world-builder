924ad092bbe2c4c83ff20cc9a1d8323835012b20
George W Langham <georgelangham1990@gmail.com>
2020-10-06 14:48:48 +0100

    basics cardroyal

--- src/components/card/CardRoyal.js
+++ src/components/card/CardRoyal.js
@@ -1,15 +1,25 @@
 import React from "react";
+const red = ["diamond", "heart"];
 function getCardFace(suit, number) {
+  let card = `${suit}-`;
   switch (number) {
     case 10:
+      card += "JACK";
       break;
     case 11:
+      card += "QUEEN";
       break;
     case 12:
+      card += "KING";
+      break;
+    case 13:
+      const colour = red.includes(suit) ? "red" : "black";
+      card = `${colour}-JOKER`;
       break;
     default:
       break;
   }
+  return card;
 }
 const CardRoyal = ({ suit, number }) => <div>{getCardFace(suit, number)}</div>;
 export default CardRoyal;
